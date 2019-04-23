95cda2f9070ed8fa56cc5ab0b44ed9ce5da4ac0e
wafambarki49 <wafa.mbarki@esprit.tn>
2019-04-23 23:42:49 +0200

    add Routes and notFoundPageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostsHomeComponent } from './posts-home/posts-home.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsHomeComponent,
-    PostDetailComponent
+    PostDetailComponent,
+    NotFoundPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
