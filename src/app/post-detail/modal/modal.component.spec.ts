3134a41046b8a81ab1b3d3a5cb4f1d366efe71aa
wafambarki49 <wafa.mbarki@esprit.tn>
2019-04-24 22:58:03 +0200

    add some Test of component Binding

--- src/app/post-detail/modal/modal.component.spec.ts
+++ src/app/post-detail/modal/modal.component.spec.ts
@@ -1,25 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
 import { ModalComponent } from './modal.component';
+import { MzModalModule, MzInputModule, MzButtonModule } from 'ngx-materialize';
+import { FormsModule } from '@angular/forms';
 
-describe('ModalComponent', () => {
-  let component: ModalComponent;
-  let fixture: ComponentFixture<ModalComponent>;
+describe('Component: Modal', () => {
 
-  beforeEach(async(() => {
+  beforeEach( () => {
     TestBed.configureTestingModule({
-      declarations: [ ModalComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      imports:[
+        MzModalModule,
+        FormsModule,
+        MzInputModule,
+        MzButtonModule
+      ],
+      declarations: [ModalComponent]
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the ModalComponent',() => {
+    let fixture = TestBed.createComponent(ModalComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
   });
+
+  it('mz-modal-header should display add Title',() => {
+    let fixture = TestBed.createComponent(ModalComponent);
+    let app = fixture.debugElement.componentInstance;
+    let modalHeader = fixture.nativeElement.querySelector('mz-modal-header');
+    expect(modalHeader.textContent).toContain('Add');
+  });
+
+  it('Modal should Have three inputs',() => {
+    let fixture = TestBed.createComponent(ModalComponent);
+    let app = fixture.debugElement.componentInstance;
+    let inputs = fixture.nativeElement.querySelectorAll('input');
+    expect(inputs.length).toBe(3);
+  });
+
+  it('Modal should Have Two buttons',() => {
+    let fixture = TestBed.createComponent(ModalComponent);
+    let app = fixture.debugElement.componentInstance;
+    let buttons = fixture.nativeElement.querySelectorAll('button');
+    expect(buttons.length).toBe(2);
+  });
+
 });
